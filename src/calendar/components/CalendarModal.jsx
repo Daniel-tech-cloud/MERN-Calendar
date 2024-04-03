90468c696eae7f8fda7a92e83961d31f05c1e36d
Daniel <guardians01@outlook.com>
2024-04-03 11:38:11 -0600

    Actualizando Modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
+import { addHours } from 'date-fns';
+
 import Modal from 'react-modal';
+import DatePicker from 'react-datepicker';
+
 import './CalendarModalStyles.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 const customStyles = {
     content: {
@@ -17,11 +22,34 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
 
-  const [isOpen, setIsOpen] = useState( true );
+    const [isOpen, setIsOpen] = useState( true );
+    const [formValue, setFormValues] = useState({
+        title: 'Daniel',
+        notes: 'Barrera',
+        start: new Date(),
+        end: addHours(new Date(), 2),
 
+    });
+
+    const onInputChanged = ( { target } ) => {
+        setFormValues({
+            ...formValue,
+            [ target.name ]: target.value
+        })
+    }
+    
     const onCloseModal = () =>{
         setIsOpen( false );
-    } 
+    }
+
+    const onDateChanged  = ( event, changing ) => {
+        setFormValues({
+            ...formValue, 
+            [changing]: event
+        })
+
+    }
+
 
     return (
         <Modal 
@@ -34,10 +62,70 @@ export const CalendarModal = () => {
             closeTimeoutMS={ 200 }
 
         >
-            <h1> Hola, mundo </h1>
+            <h1> Nuevo evento </h1>
             <hr />
-            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia maxime error itaque illum corporis temporibus, minima labore quo, atque, placeat alias? Error voluptate omnis saepe possimus delectus, deleniti fugiat nesciunt?</p>
-        
+            <form className="container">
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label>
+                    <DatePicker 
+                    minDate={ formValue.start }
+                        selected={ formValue.start }
+                        onChange={ ( event ) => onDateChanged(event, 'start') }
+                        className="form-control"
+                        dateFormat="Pp"
+
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label>
+                    <DatePicker
+                        selected={ formValue.end }
+                        onChange={ ( event ) => onDateChanged(event, 'end') }
+                        className="form-control"
+                        dateFormat="Pp"
+                        
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={ formValue.title }
+                        onChange={ onInputChanged }
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea 
+                        type="text" 
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={ formValue.notes }
+                        onChange={ onInputChanged }
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
         </Modal>
         
 
